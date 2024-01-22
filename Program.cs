2c52eda148f24f11ccd815de03fb6868828a5c65
Глеб <@HOME-PC>
2024-01-22 20:07:34 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if(a>=0&a<=60)
+            {
+                Console.WriteLine("НЕУДОВЛЕТВОРИТЕЛЬНО");
+            }
+            else if(a>60&a<=74)
+            {
+                Console.WriteLine("УДОВЛЕТВОРИТЕЛЬНО");
+            }
+             else if(a>74&a<=89)
+            {
+                Console.WriteLine("ХОРОШО");
+            }
+            else if(a>89&a<=100)
+            {
+                Console.WriteLine("ОТЛИЧНО");
+            }
+
+            Console.ReadKey();
+            
+        }
+
+    }
+}
